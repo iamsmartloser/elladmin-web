d3f1b6ecd357b2765c91480b5dc71c3deb206eee
iamsmartloser <121484818@qq.com>
2021-04-06 18:03:52 +0800

    初始化

--- src/mixins/map.js
+++ src/mixins/map.js
@@ -1,22 +1,22 @@
-import { constant } from '@riophae/vue-treeselect/src/utils'
-
 const BMap = window.BMapGL
 const COLOR_CONFIG = {
-  disabledColor: '#595959',
-  noParkingColor: '#ff0000', // 禁停区颜色
-  noReturnColor: '#ff800f', // 禁还区颜色
-  blindAreaColor: '#808080', // 信号盲区颜色
-  fenceColor: '#2ba045', // 禁停区颜色
-  trailColor: '#1869AD', // 车辆轨迹颜色
-  originTrailColor: '#f9c700', // 车辆原始轨迹颜色
-  operationColor: '#1890ff', // 运维区颜色
-  vehicleAreaColor: '#CDCD00', // 车辆位置区区颜色
-  targetAreaColor: '#98FB98' // 车辆位置区区颜色
+  'disabledColor': '#595959',
+  'noParkingColor': '#ff0000', // 禁停区颜色
+  'noReturnColor': '#ff800f', // 禁还区颜色
+  'blindAreaColor': '#808080', // 信号盲区颜色
+  'fenceColor': '#2ba045', // 禁停区颜色
+  'trailColor': '#1869AD', // 车辆轨迹颜色
+  'originTrailColor': '#f9c700', // 车辆原始轨迹颜色
+  'operationColor': '#1890ff', // 运维区颜色
+  'vehicleAreaColor': '#CDCD00', // 车辆位置区区颜色
+  'targetAreaColor': '#98FB98' // 车辆位置区区颜色
 }
 export default {
   data() {
     return {
-      map: null
+      map: null,
+      drawingManager: null,
+      radiusLabel: null
     }
   },
   methods: {
@@ -25,19 +25,28 @@ export default {
       this.map = map
       console.log('ready:', map)
     },
+    drawMarker(point, markerOptions) {
+      const marker = new BMap.Marker(new BMap.Point(point.lng, point.lat), markerOptions)
+      this.map.addOverlay(marker)
+      return marker
+    },
     drawPolyline(points, polylineOptions) {
+      console.log('drawPolyline begin')
       const strokeColor = COLOR_CONFIG[polylineOptions.strokeColor]
+      console.log('strokeColor:', strokeColor)
       // 设置默认的参数
       const option = {
-        strokeWeight: polylineOptions.strokeWeight || 1, // 边线的宽度，以像素为单位。
+        strokeWeight: polylineOptions.strokeWeight || 2, // 边线的宽度，以像素为单位。
         strokeOpacity: polylineOptions.strokeOpacity || 1, // 边线透明度，取值范围0 - 1。
-        strokeStyle: polylineOptions.strokeStyle || 'solid' , // 边线的样式，solid或dashed。
+        strokeStyle: polylineOptions.strokeStyle || 'solid', // 边线的样式，solid或dashed。
         strokeColor, // 边线的颜色
         enableEditing: polylineOptions.enableEditing || false, // 是否启用编辑，默认不启用
         enableClicking: polylineOptions.enableClicking || true // 是否响应点击事件，默认为true
       }
-      const polyline = new BMap.Polyline(points, option)
+
+      const polyline = new BMap.Polyline(points.map(p => { return new BMap.Point(p.lng, p.lat) }), option)
       this.map.addOverlay(polyline)
+      console.log('drawPolyline end')
       return polyline
     },
     drawPolygon(points, polygonOptions) {
@@ -45,34 +54,97 @@ export default {
       const fillColor = COLOR_CONFIG[polygonOptions.fillColor]
       // 设置默认的参数
       const option = {
-        strokeWeight: polygonOptions.strokeWeight || 1, // 边线的宽度，以像素为单位。
+        strokeWeight: polygonOptions.strokeWeight || 2, // 边线的宽度，以像素为单位。
         strokeOpacity: polygonOptions.strokeOpacity || 1, // 边线透明度，取值范围0 - 1。
-        strokeStyle: polygonOptions.strokeStyle || 'solid' , // 边线的样式，solid或dashed。
+        strokeStyle: polygonOptions.strokeStyle || 'solid', // 边线的样式，solid或dashed。
         strokeColor, // 边线的颜色
         fillColor, // 填充颜色 当参数为空时，圆形将没有填充效果。
         fillOpacity: 0.3, // 填充的透明度，取值范围0 - 1。
         enableEditing: polygonOptions.enableEditing || false, // 是否启用编辑，默认不启用
         enableClicking: polygonOptions.enableClicking || true // 是否响应点击事件，默认为true
       }
-      const polygon = new BMap.Polygon(points, option)
+      const polygon = new BMap.Polygon(points.map(p => { return new BMap.Point(p.lng, p.lat) }), option)
+      this.map.addOverlay(polygon)
       return polygon
     },
-    drawCircle(points, circleOptions) {
+    drawCircle(point, radius, circleOptions) {
+      const r = radius || 20
       const strokeColor = COLOR_CONFIG[circleOptions.strokeColor]
       const fillColor = COLOR_CONFIG[circleOptions.fillColor]
       // 设置默认的参数
       const option = {
         strokeWeight: circleOptions.strokeWeight || 1, // 边线的宽度，以像素为单位。
         strokeOpacity: circleOptions.strokeOpacity || 1, // 边线透明度，取值范围0 - 1。
-        strokeStyle: circleOptions.strokeStyle || 'solid' , // 边线的样式，solid或dashed。
+        strokeStyle: circleOptions.strokeStyle || 'solid', // 边线的样式，solid或dashed。
         strokeColor, // 边线的颜色
         fillColor, // 填充颜色 当参数为空时，圆形将没有填充效果。
         fillOpacity: 0.3, // 填充的透明度，取值范围0 - 1。
         enableEditing: circleOptions.enableEditing || false, // 是否启用编辑，默认不启用
         enableClicking: circleOptions.enableClicking || true // 是否响应点击事件，默认为true
       }
-      const circle = new BMap.Circle(points, option)
+      const circle = new BMap.Circle(new BMap.Point(point.lng, point.lat), r, option)
+
+      this.map.addOverlay(circle)
+      this.radiusLabel = new BMap.Label('半径:' + r, {})
+      console.log('vertexMarkers:', circle)
+      console.log('circle:', circle.vertexMarkers['1'])
+      // if (circle.vertexMarkers[0].setLabel) {
+      //   circle.vertexMarkers[0].setLabel(this.radiusLabel)
+      // }
+      // // 如果允许编辑
+      // if (option.enableEditing) {
+      //   circle.addEventListener('lineupdate', (e) => {
+      //     console.log('lineupdate:', e.overlay)
+      //     console.log('lineupdate latLng:', e.overlay)
+      //     console.log('lineupdate radius:', e.overlay.radius)
+      //     if (!this.radiusLabel) {
+      //       this.radiusLabel = new BMap.Label('半径:' + e.overlay.radius, {})
+      //     } else {
+      //       this.map.removeOverlay(this.radiusLabel)
+      //       this.radiusLabel = new BMap.Label('半径:' + e.overlay.radius, {})
+      //     }
+      //     e.overlay.vertexMarkers[1].setLabel(this.radiusLabel)
+      //
+      //     //
+      //   })
+      // }
+
       return circle
     },
+    // setDrawingMode:BMAP_DRAWING_MARKER\BMAP_DRAWING_POLYLINE\
+    // BMAP_DRAWING_RECTANGLE\BMAP_DRAWING_POLYGON\BMAP_DRAWING_CIRCLE
+    createDrawingManager() {
+      // const styleOptions = {
+      //   strokeColor: '#5E87DB', // 边线颜色
+      //   fillColor: '#5E87DB', // 填充颜色。当参数为空时，圆形没有填充颜色
+      //   strokeWeight: 2, // 边线宽度，以像素为单位
+      //   strokeOpacity: 1, // 边线透明度，取值范围0-1
+      //   fillOpacity: 0.2 // 填充透明度，取值范围0-1
+      // }
+      // const labelOptions = {
+      //   borderRadius: '2px',
+      //   background: '#FFFBCC',
+      //   border: '1px solid #E1E1E1',
+      //   color: '#703A04',
+      //   fontSize: '12px',
+      //   letterSpacing: '0',
+      //   padding: '5px'
+      // }
+      // // 实例化鼠标绘制工具
+      // if (!this.drawingManager) {
+      //   this.drawingManager = new window.BMapGLLib.DrawingManager(this.map, {
+      //     // isOpen: true,        // 是否开启绘制模式
+      //     enableCalculate: false, // 绘制是否进行测距测面
+      //     enableSorption: false, // 是否开启边界吸附功能
+      //     // sorptiondistance: 20, // 边界吸附距离
+      //     circleOptions: styleOptions, // 圆的样式
+      //     polylineOptions: styleOptions, // 线的样式
+      //     polygonOptions: styleOptions, // 多边形的样式
+      //     rectangleOptions: styleOptions, // 矩形的样式
+      //     labelOptions: labelOptions // label样式
+      //   })
+      // }
+      // return this.drawingManager
+    }
   }
 }
--- src/views/test/jsmap/index.vue
+++ src/views/test/jsmap/index.vue
@@ -36,23 +36,91 @@ export default {
   mixins: [map_mixins],
   data() {
     return {
-      polyline: null
+      polyline: null,
+      polylinePoints: [],
+      marker: null,
+      polygonPoints: [],
+      polygon: null
     }
   },
   mounted() {
-    console.log('drawPolyline')
-    console.log('this.map:', this.map)
-    this.map.addEventListener('click', (e) => {
-      console.log('click:', e)
-      if (!this.polyline) {
-        this.polyline = this.drawPolyline([e.point], { strokeColor: '#555' })
-      } else {
-        this.polyline.setPath([...this.polyline.getPath(), e.point])
-      }
-    })
+    // console.log('drawPolyline')
+    // console.log('this.map:', this.map)
+    this.map.setDefaultCursor('pointer') // 设置鼠标默认的指针样式为手
+    // this.editPolyline();
+    // this.editPolygon()
+    this.editCircle()
   },
   methods: {
-
+    editPolyline() {
+      this.map.addEventListener('click', (e) => {
+        console.log('click:', e)
+        if (this.polylinePoints.length === 0) {
+          const p = { lng: e.latlng.lng, lat: e.latlng.lat }
+          this.polylinePoints.push(p)
+          this.marker = this.drawMarker(p)
+          this.marker.enableDragging()
+        } else {
+          if (this.marker) {
+            this.map.removeOverlay(this.marker)
+            this.marker = null
+          } else {
+            this.map.removeOverlay(this.polyline)
+          }
+          this.polylinePoints.push({ lng: e.latlng.lng, lat: e.latlng.lat })
+          this.polyline = this.drawPolyline(this.polylinePoints, { strokeColor: 'fenceColor' })
+          this.polyline.enableEditing()
+          console.log('drawPolyline after:', this.polyline)
+        }
+      })
+    },
+    editPolygon() {
+      this.map.addEventListener('click', (e) => {
+        console.log('click:', e)
+        if (this.polygonPoints.length === 0) {
+          const p = { lng: e.latlng.lng, lat: e.latlng.lat }
+          this.polygonPoints.push(p)
+          this.marker = this.drawMarker(p)
+          this.marker.enableDragging()
+        } else {
+          if (this.marker) {
+            this.map.removeOverlay(this.marker)
+            this.marker = null
+          } else {
+            this.map.removeOverlay(this.polygon)
+          }
+          this.polygonPoints.push({ lng: e.latlng.lng, lat: e.latlng.lat })
+          this.polygon = this.drawPolygon(this.polygonPoints, { strokeColor: 'fenceColor' })
+          this.polygon.enableEditing()
+          console.log('polygonPoints after:', this.polygon)
+        }
+      })
+    },
+    editCircle() {
+      this.map.addEventListener('click', (e) => {
+        console.log('click:', e)
+          if (!this.circle) {
+          const p = { lng: e.latlng.lng, lat: e.latlng.lat }
+          this.circle = this.drawCircle(p, null, { strokeColor: 'fenceColor', enableEditing: true })
+          // this.circle.enableEditing()
+          //   // 编辑事件回调
+          // this.circle.addEventListener('lineupdate',(e)=>{
+          //   console.log('lineupdate:',e.overlay.vertexMarkers[0].latLng)
+          //   console.log('lineupdate latLng:',e.overlay.latLng)
+          //   console.log('lineupdate radius:',e.overlay.radius)
+          // })
+          // console.log('editCircle after:', this.circle)
+        }
+      })
+    },
+    // drawByManager() {
+    //   this.createDrawingManager()
+    //   this.drawingManager.setDrawingMode(window.BMAP_DRAWING_CIRCLE)
+    //   this.drawingManager.open()
+    //   this.drawingManager.addEventListener('circlecomplete', function(overlay) {
+    //     console.log('circlecomplete:', overlay)
+    //   })
+    // }
   }
 }
 </script>
